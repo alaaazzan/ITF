29f13aafe3018e1cd7979ef4fd327bc0bd501acb
ITF <alaaa.azzam73@gmail.com>
2023-09-17 14:11:13 +0300

    second commit

--- main.py
+++ main.py
@@ -12,10 +12,55 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-print ("Alaa")
+#print ("Alaa")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("Ali")
-if 10 > 2:
-    print ("Yes")
+#print("Ali")
+#if 10 > 2:
+ #   print ("Yes")
 
-print ("no")
+#print ("no")
+#TODO Tsak
+
+#print ("Enter num1")
+#input("Enter num1 =")
+#age=True
+#age=int(age)
+#print (age)
+#print(type(age))
+
+
+#my_name=None
+##num1=input("Enter num1 = ")
+#num2=input("Enter num2 = ")
+#print (num1,num2)
+#print (type(age))
+
+#first_name=input("Enter first name : ")
+#last_name=input("Enter last name : ")
+#paragraph=f"""
+#>>>name : {first_name} {last_name}
+#1_nationality is palestainian
+
+#"""
+#print (paragraph)
+
+x="  Hello World         "
+#print (x[2:-2])
+#print (x.upper())
+#print (x.lower())
+#print (x.title())
+#print (len(x))
+#print (x.strip())
+#print (x.replace("l","*"))
+#print (x.replace(x[2:-2]))
+#print (x.split())
+#x="Alaa"
+#y=5
+
+#print("m"not in x)
+
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
---
+++ task 3/task3.py
@@ -0,0 +1,24 @@
+def add(x,y):
+    total=x+y
+    print(total)
+
+add (10,5)
+
+def sub(x,y):
+    total=x-y
+    print(total)
+
+sub (10,5)
+
+
+def div(x,y):
+    total=x//y
+    print(total)
+
+div (10,5)
+
+def mul(x,y):
+    total=x*y
+    print(total)
+
+mul (10,5)
